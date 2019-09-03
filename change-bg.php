b059d386e376c638916d5f932a2068a31b9bbf3a
travis <travis@travis-ci.org>
2019-09-03 18:46:59 +0200

    Init

---
+++ change-bg.php
@@ -0,0 +1,12 @@
+<?php 
+
+/**
+ * Plugin Name: Panel to change background color.
+ * Plugin URI: https://michalsmietana.pl
+ * Description: Panel, Who changing background color.
+ * Version: 1.0.0
+ * Author: Michal Smietana
+ * Author URI: http://michalsmietana.com
+ * */
+
+?>
