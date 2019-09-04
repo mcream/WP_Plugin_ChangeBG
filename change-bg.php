fe4333b287270cb9b1a4142af9727879ba3154e3
travis <travis@travis-ci.org>
2019-09-04 11:08:43 +0200

    added new file index.php and insert define operation

--- change-bg.php
+++ change-bg.php
@@ -6,7 +6,30 @@
  * Description: Panel, Who changing background color.
  * Version: 1.0.0
  * Author: Michal Smietana
- * Author URI: http://michalsmietana.com
+ * Author URI: http://michalsmietana.pl
  * */
+
+ // Activation plugin
+register_activation_hook( __FILE__, 'changebg_activation' );
+function changebg_activation() {
+
+}
+
+// Deactivation plugin
+register_deactivation_hook( __FILE__, 'changebg_deactivation' );
+function changebg_deactivation() {
+
+}
+
+// Uninstall plugin
+register_uninstall_hook( __FILE__, 'changebg_uninstall' );
+function changebg_uninstall() {
+    if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+        exit;
+    }
+}
+
+
+
 
 ?>
