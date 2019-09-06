f0eabc35362b30234800ccddda75a18a9440c062
travis <travis@travis-ci.org>
2019-09-06 12:15:52 +0200

    added hooks, set up cpt, create constructor

--- change-bg.php
+++ change-bg.php
@@ -11,22 +11,34 @@
  * Author URI: http://michalsmietana.pl
  * */
 
-defined( 'ABSPATH' ) or die( 'Hey, you can\t access this file, you silly human!' );
+defined( 'ABSPATH' ) or die( 'You can not access to this file. Try Again.' );
 
 class ChangeBG
 {
+    function __construct(){
+        add_action( 'init', array( $this, 'custom_post_type' ) );
+    }
     function activate(){
-
+        echo 'Plugin został poprawnie zainstalowany';
     }
     function deactivate(){
-
+        echo 'Plugin NIE został poprawnie zainstalowany';
     }
     function uninstall(){
 
+    }
+    function custom_post_type(){
+        register_post_type( 'posty', ['public' => true, 'label' => 'Posty'] );
     }
 }
 if ( class_exists( 'ChangeBG' ) ){
     $changeBG = new ChangeBG();
 }
+
+//acti
+register_activation_hook(__FILE__, array( $changeBG, 'activate' ));
+//deacti
+register_deactivation_hook(__FILE__, array( $changeBG, 'deactivate' ));
+//unin
 
 ?>
